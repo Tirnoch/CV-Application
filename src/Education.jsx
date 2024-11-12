eb95e5e483c47453ed40eb442b0ea79eaf56701b
firat <firat.duzgun@hotmail.com>
2024-11-12 16:34:45 +0300

    full sidebar

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,7 +1,71 @@
+import { useState } from 'react';
+import './box.css';
+
 function Education() {
+  const [school, setSchool] = useState({
+    name: 'Mimar Sinan University',
+    degree: 'Sanat Sepet Isleri',
+    start: '',
+    end: '',
+  });
+  const handleName = (e) => {
+    setSchool({ ...school, name: e.target.value });
+  };
+  const handleDegree = (e) => {
+    setSchool({ ...school, degree: e.target.value });
+  };
+  const handleStart = (e) => {
+    setSchool({ ...school, start: e.target.value });
+  };
+  const handleEnd = (e) => {
+    setSchool({ ...school, end: e.target.value });
+  };
+
   return (
     <>
-      <h2>Education</h2>
+      <div className="container">
+        <h2>Education</h2>
+        <div className="segment">
+          <label htmlFor="name">Full Name: </label>
+          <input
+            type="text"
+            value={school.name}
+            onChange={handleName}
+            id="name"
+          />
+        </div>
+        <br />
+        <div className="segment">
+          <label htmlFor="degree">Degree: </label>
+          <input
+            type="text"
+            value={school.degree}
+            onChange={handleDegree}
+            id="degree"
+          />
+        </div>
+        <br />
+        <div className="segment">
+          <label htmlFor="start-date">Start Date: </label>
+          <input
+            type="date"
+            value={school.start}
+            onChange={handleStart}
+            id="start-date"
+          />
+        </div>
+        <br />
+        <div className="segment">
+          <label htmlFor="end-date">End Date: </label>
+          <input
+            type="date"
+            value={school.end}
+            onChange={handleEnd}
+            id="end-date"
+          />
+        </div>
+        <br />
+      </div>
     </>
   );
 }
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,84 @@
+import { useState } from 'react';
+import './box.css';
+
 function Experience() {
+  const [experience, setExperience] = useState({
+    name: 'Odunluk Graphic Design',
+    title: 'Graphic Designer',
+    start: '',
+    end: '',
+    description: '',
+  });
+  const handleName = (e) => {
+    setExperience({ ...experience, name: e.target.value });
+  };
+  const handleDegree = (e) => {
+    setExperience({ ...experience, title: e.target.value });
+  };
+  const handleStart = (e) => {
+    setExperience({ ...experience, start: e.target.value });
+  };
+  const handleEnd = (e) => {
+    setExperience({ ...experience, end: e.target.value });
+  };
+  const handleDescription = (e) => {
+    setExperience({ ...experience, description: e.target.value });
+  };
+
   return (
     <>
-      <h2>Experience</h2>
+      <div className="container">
+        <h2>Experience</h2>
+        <div className="segment">
+          <label htmlFor="name">Full Name: </label>
+          <input
+            type="text"
+            value={experience.name}
+            onChange={handleName}
+            id="name"
+          />
+        </div>
+        <br />
+        <div className="segment">
+          <label htmlFor="degree">Title: </label>
+          <input
+            type="text"
+            value={experience.degree}
+            onChange={handleDegree}
+            id="degree"
+          />
+        </div>
+        <br />
+        <div className="segment">
+          <label htmlFor="start-date">Start Date: </label>
+          <input
+            type="date"
+            value={experience.start}
+            onChange={handleStart}
+            id="start-date"
+          />
+        </div>
+        <br />
+        <div className="segment">
+          <label htmlFor="end-date">End Date: </label>
+          <input
+            type="date"
+            value={experience.end}
+            onChange={handleEnd}
+            id="end-date"
+          />
+        </div>
+        <br />
+        <div className="segment">
+          <label htmlFor="degree">Description: </label>
+          <textarea
+            rows="5"
+            value={experience.description}
+            onChange={handleDescription}
+            id="degree"
+          />
+        </div>
+      </div>
     </>
   );
 }
